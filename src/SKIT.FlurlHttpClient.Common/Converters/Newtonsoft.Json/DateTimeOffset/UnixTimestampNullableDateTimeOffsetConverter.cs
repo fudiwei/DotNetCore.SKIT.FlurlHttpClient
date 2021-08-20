b1c0b1545a862e94c80409f3a734c54270777c93
Fu Diwei <fudiwei@sina.com>
2021-08-20 18:44:30 +0800

    feat: 新增 DateTimeOffset 转换成时间戳形式的 JsonConverter

---
+++ src/SKIT.FlurlHttpClient.Common/Converters/Newtonsoft.Json/DateTimeOffset/UnixTimestampNullableDateTimeOffsetConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Newtonsoft.Json.Converters
+{
+    public class UnixTimestampNullableDateTimeOffsetConverter : JsonConverter<DateTimeOffset?>
+    {
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return true; }
+        }
+
+        public override DateTimeOffset? ReadJson(JsonReader reader, Type objectType, DateTimeOffset? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return existingValue;
+            } 
+            else if (reader.TokenType == JsonToken.Integer)
+            {
+                long value = serializer.Deserialize <long>(reader);
+                return DateTimeOffset.FromUnixTimeSeconds(value);
+            }
+
+            throw new JsonReaderException();
+        }
+
+        public override void WriteJson(JsonWriter writer, DateTimeOffset? value, JsonSerializer serializer)
+        {
+            if (value.HasValue)
+                writer.WriteValue(value.Value.ToUnixTimeSeconds());
+            else
+                writer.WriteNull();
+        }
+    }
+}
---
+++ src/SKIT.FlurlHttpClient.Common/Converters/System.Text.Json/DateTimeOffset/UnixTimestampNullableDateTimeOffsetConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace System.Text.Json.Converters
+{
+    public class UnixTimestampNullableDateTimeOffsetConverter : JsonConverter<DateTimeOffset?>
+    {
+        public override DateTimeOffset? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Null)
+            {
+                return null;
+            }
+            else if (reader.TokenType == JsonTokenType.Number)
+            {
+                long value = reader.GetInt64();
+                return DateTimeOffset.FromUnixTimeSeconds(value);
+            }
+
+            throw new JsonException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTimeOffset? value, JsonSerializerOptions options)
+        {
+            if (value.HasValue)
+                writer.WriteNumberValue(value.Value.ToUnixTimeSeconds());
+            else
+                writer.WriteNullValue();
+        }
+    }
+}
