e5638f2018073f084bd5750b344232968ccc9bab
Fu Diwei <fudiwei@sina.com>
2022-01-04 17:33:22 +0800

    feat: 增加包装响应结果的公共方法

--- src/SKIT.FlurlHttpClient.Common/ICommonResponse.cs
+++ src/SKIT.FlurlHttpClient.Common/ICommonResponse.cs
@@ -13,21 +13,21 @@ namespace SKIT.FlurlHttpClient
         /// </summary>
         [Newtonsoft.Json.JsonIgnore]
         [System.Text.Json.Serialization.JsonIgnore]
-        public int RawStatus { get; }
+        public int RawStatus { get; internal set; }
 
         /// <summary>
         /// 获取原始的 HTTP 响应表头集合。
         /// </summary>
         [Newtonsoft.Json.JsonIgnore]
         [System.Text.Json.Serialization.JsonIgnore]
-        public IDictionary<string, string> RawHeaders { get; }
+        public IDictionary<string, string> RawHeaders { get; internal set; }
 
         /// <summary>
         /// 获取原始的 HTTP 响应正文。
         /// </summary>
         [Newtonsoft.Json.JsonIgnore]
         [System.Text.Json.Serialization.JsonIgnore]
-        public byte[] RawBytes { get; }
+        public byte[] RawBytes { get; internal set; }
 
         /// <summary>
         /// 获取一个值，该值指示调用 API 是否成功。
