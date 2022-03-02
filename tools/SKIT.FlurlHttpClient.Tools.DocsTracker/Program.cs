c7c3c187e50ab10f7c31681606e9b8d2ba327757
Fu Diwei <fudiwei@sina.com>
2022-03-02 21:10:12 +0800

    test

--- tools/SKIT.FlurlHttpClient.Tools.DocsTracker/Program.cs
+++ tools/SKIT.FlurlHttpClient.Tools.DocsTracker/Program.cs
@@ -7,10 +7,13 @@ namespace SKIT.FlurlHttpClient.Tools.DocsTracker
     {
         static async Task Main(string[] args)
         {
-            //new Trackers.WeixinMiniProgramBackendTracker(new DocsTrackerOptions() { OutputPath = "./Logs/Weixin.MiniProgram.Backend/" }).RunAsync().Wait();
-            //new Trackers.WeixinMiniProgramPlatformTracker(new DocsTrackerOptions() { OutputPath = "./Logs/Weixin.MiniProgram.Platform/" }).RunAsync().Wait();
-            new Trackers.WeixinPayCommonTracker(new DocsTrackerOptions() { OutputPath = "./Logs/Weixin.Pay.Common/" }).RunAsync().Wait();
-            //new Trackers.WeixinPayPartnerTracker(new DocsTrackerOptions() { OutputPath = "./Logs/Weixin.Pay.Partner/" }).RunAsync().Wait();
+            const string WORD_DIR = "./Logs";
+            Task.WaitAll(
+                new Trackers.WeixinMiniProgramBackendTracker(new DocsTrackerOptions() { OutputPath = $"{WORD_DIR}/Weixin.MiniProgram.Backend" }).RunAsync(),
+                new Trackers.WeixinMiniProgramPlatformTracker(new DocsTrackerOptions() { OutputPath = $"{WORD_DIR}/Weixin.MiniProgram.Platform" }).RunAsync(),
+                new Trackers.WeixinPayCommonTracker(new DocsTrackerOptions() { OutputPath = $"{WORD_DIR}/Weixin.Pay.Common" }).RunAsync(),
+                new Trackers.WeixinPayPartnerTracker(new DocsTrackerOptions() { OutputPath = $"{WORD_DIR}/Weixin.Pay.Partner" }).RunAsync()
+            );
         }
     }
 }
