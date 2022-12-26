236a3faae0d8f93eb12d519ae89b7860c468d23c
fudiwei <fudiwei@sina.com>
2022-12-26 22:13:11 +0800

    feat(core): 新增部分特化的异常类型

---
+++ src/SKIT.FlurlHttpClient.Common/Exceptions/CommonSerializationException.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace SKIT.FlurlHttpClient.Exceptions
+{
+    /// <summary>
+    /// 表示 SKIT.FlurlHttpClient 数据序列化或反序列化失败引发的异常。
+    /// </summary>
+    public class CommonSerializationException : CommonException
+    {
+        /// <inheritdoc/>
+        public CommonSerializationException()
+        {
+        }
+
+        /// <inheritdoc/>
+        public CommonSerializationException(string message)
+            : base(message)
+        {
+        }
+
+        /// <inheritdoc/>
+        public CommonSerializationException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ src/SKIT.FlurlHttpClient.Common/Exceptions/CommonTimeoutException.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace SKIT.FlurlHttpClient.Exceptions
+{
+    /// <summary>
+    /// 表示 SKIT.FlurlHttpClient 请求超时引发的异常。
+    /// </summary>
+    public class CommonTimeoutException : CommonException
+    {
+        /// <inheritdoc/>
+        public CommonTimeoutException()
+        {
+        }
+
+        /// <inheritdoc/>
+        public CommonTimeoutException(string message)
+            : base(message)
+        {
+        }
+
+        /// <inheritdoc/>
+        public CommonTimeoutException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
