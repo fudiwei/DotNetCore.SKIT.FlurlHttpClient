a80e951f752d3f97e3b94d25cbf66bdac82b3503
Fu Diwei <fudiwei@sina.com>
2023-10-28 22:04:03 +0800

    feat(core): 支持配置客户端默认的 HTTP 协议版本号

--- src/SKIT.FlurlHttpClient.Common/CommonClientSettings.cs
+++ src/SKIT.FlurlHttpClient.Common/CommonClientSettings.cs
@@ -12,31 +12,38 @@ namespace SKIT.FlurlHttpClient
     public sealed class CommonClientSettings
     {
         /// <summary>
-        ///
+        /// 获取或设置客户端默认请求超时时间间隔。
         /// </summary>
         public TimeSpan? Timeout { get; set; }
 
         /// <summary>
-        ///
+        /// 获取或设置客户端默认的 HTTP 协议版本号。
+        /// </summary>
+        public Version HttpVersion { get; set; } = default!;
+
+        /// <summary>
+        /// 获取或设置客户端用于序列化 "application/json" 内容的序列化器。
         /// </summary>
         public IJsonSerializer JsonSerializer { get; set; } = default!;
 
         /// <summary>
-        ///
+        /// 获取或设置客户端用于序列化 "application/x-www-form-urlencoded" 内容的序列化器。
         /// </summary>
         public IFormUrlEncodedSerializer FormUrlEncodedSerializer { get; set; } = default!;
 
+        // TODO: Migrate to Flurl.Http v4.x.
         /// <summary>
-        ///
+        /// 
         /// </summary>
         public IHttpClientFactory FlurlHttpClientFactory { get; set; } = default!;
 
-        internal CommonClientSettings(ClientFlurlHttpSettings flurlClientSettings)
+        internal CommonClientSettings(FlurlHttpSettings flurlSettings)
         {
-            Timeout = flurlClientSettings.Timeout;
-            JsonSerializer = ((InternalWrappedJsonSerializer)flurlClientSettings.JsonSerializer)!.Serializer;
-            FormUrlEncodedSerializer = ((InternalWrappedFormUrlEncodedSerializer)flurlClientSettings.UrlEncodedSerializer)!.Serializer;
-            FlurlHttpClientFactory = flurlClientSettings.HttpClientFactory;
+            Timeout = flurlSettings.Timeout;
+            HttpVersion = Version.Parse(flurlSettings.HttpVersion);
+            JsonSerializer = ((InternalWrappedJsonSerializer)flurlSettings.JsonSerializer)!.Serializer;
+            FormUrlEncodedSerializer = ((InternalWrappedFormUrlEncodedSerializer)flurlSettings.UrlEncodedSerializer)!.Serializer;
+            FlurlHttpClientFactory = flurlSettings.HttpClientFactory;
         }
     }
 }
