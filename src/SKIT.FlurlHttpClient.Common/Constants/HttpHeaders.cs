0763c26c6afe4100a220ae2cc3c54729a51492f8
Fu Diwei <fudiwei@sina.com>
2024-03-08 23:29:23 +0800

    feat(core): 补充更多 HTTP 标头常量

--- src/SKIT.FlurlHttpClient.Common/Constants/HttpHeaders.cs
+++ src/SKIT.FlurlHttpClient.Common/Constants/HttpHeaders.cs
@@ -5,6 +5,8 @@ namespace SKIT.FlurlHttpClient
     /// </summary>
     public static class HttpHeaders
     {
+        public const string Allow = "Allow";
+
         public const string Accept = "Accept";
 
         public const string AcceptCharset = "Accept-Charset";
@@ -39,6 +41,8 @@ namespace SKIT.FlurlHttpClient
 
         public const string Connection = "Connection";
 
+        public const string ContentDisposition = "Content-Disposition";
+
         public const string ContentEncoding = "Content-Encoding";
 
         public const string ContentLanguage = "Content-Language";
@@ -46,6 +50,8 @@ namespace SKIT.FlurlHttpClient
         public const string ContentLength = "Content-Length";
 
         public const string ContentLocation = "Content-Location";
+
+        public const string ContentMD5 = "Content-MD5";
 
         public const string ContentRange = "Content-Range";
 
@@ -60,6 +66,8 @@ namespace SKIT.FlurlHttpClient
         public const string Date = "Date";
 
         public const string ETag = "ETag";
+
+        public const string Expect = "Expect";
 
         public const string Expires = "Expires";
 
@@ -80,6 +88,8 @@ namespace SKIT.FlurlHttpClient
         public const string LastModified = "Last-Modified";
 
         public const string Location = "Location";
+
+        public const string MaxForwards = "Max-Forwards";
 
         public const string Origin = "Origin";
 
@@ -111,6 +121,8 @@ namespace SKIT.FlurlHttpClient
 
         public const string TransferEncoding = "Transfer-Encoding";
 
+        public const string Upgrade = "Upgrade";
+
         public const string UserAgent = "User-Agent";
 
         public const string VariantVary = "Variant-Vary";
@@ -118,5 +130,9 @@ namespace SKIT.FlurlHttpClient
         public const string Vary = "Vary";
 
         public const string Via = "Via";
+
+        public const string Warning = "Warning";
+
+        public const string WWWAuthenticate = "WWW-Authenticate";
     }
 }
